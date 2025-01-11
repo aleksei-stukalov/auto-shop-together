a8265efe694ebe1d2e9ed67139d3e44129a22795
Aleksei Stukalov <stukalov.nz@gmail.com>
2025-01-11 23:52:06 +1300

    remove old code

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@components': '/src/components',
-      '@game': '/src/game',
-      '@enums': '/src/models/enums.ts',
-      '@interfaces': '/src/models/interfaces.ts',
-    },
-  },
 })
