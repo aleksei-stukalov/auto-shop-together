fbeed644657814d6fccabd8df6ec2a4d04bb041c
Aleksei Stukalov <stukalov.nz@gmail.com>
2025-01-17 23:30:51 +1300

    render floor tilemap

--- src/game/scenes/ShopScene.ts
+++ src/game/scenes/ShopScene.ts
@@ -3,11 +3,27 @@ import { GameState } from '@enums'
 export default class ShopScene extends Phaser.Scene {
   state: GameState = GameState.GAME_SCENE
 
+  floorTilemap!: Phaser.Tilemaps.Tilemap
+
   constructor() {
     super('ShopScene')
   }
 
   create() {
-    this.add.text(20, 20, 'Shop Scene')
+    this.floorTilemap = this.make.tilemap({
+      width: 800,
+      height: 600,
+      tileWidth: 32,
+      tileHeight: 32,
+    })
+
+    this.floorTilemap.addTilesetImage('dev-black', 'dev-black', 32, 32)
+    this.floorTilemap.createBlankLayer('floor', 'dev-black')
+
+    for (let y = 0; y < 20; y++) {
+      for (let x = 0; x < 25; x++) {
+        this.floorTilemap.putTileAt(0, x, y)
+      }
+    }
   }
 }
