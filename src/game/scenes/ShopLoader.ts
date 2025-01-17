2df86ee183d97de571a616cdae9d4fe340e6e871
Aleksei Stukalov <stukalov.nz@gmail.com>
2025-01-17 23:31:04 +1300

    preload floor dev texture

--- src/game/scenes/ShopLoader.ts
+++ src/game/scenes/ShopLoader.ts
@@ -3,9 +3,41 @@ import { GameState } from '@enums'
 
 export default class ShopLoader extends Phaser.Scene {
   state: GameState = GameState.GAME_LOADER
+  viewportWidth: number = window.innerWidth
+  viewportHeight: number = window.innerHeight
 
   constructor() {
     super('ShopLoader')
+  }
+
+  init() {
+    // Draw a loading bar
+    this.add.rectangle(
+      this.viewportWidth / 2,
+      this.viewportHeight / 2,
+      500,
+      3,
+      0xffffff,
+      0.5
+    )
+    // Draw a progress bar
+    const bar = this.add.rectangle(
+      this.viewportWidth / 2 - 250,
+      this.viewportHeight / 2,
+      0,
+      3,
+      0xffffff
+    )
+
+    this.load.on('progress', (value: number) => {
+      bar.width = 5 + 495 * value
+    })
+  }
+
+  preload() {
+    this.load.setPath('assets')
+
+    this.load.image('dev-black', 'tile_floor_dev-black.png')
   }
 
   create() {
